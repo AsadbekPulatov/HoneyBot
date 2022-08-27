3fa30bf48c266affbfb0c010d1fe6d7a919d2a48
Lenovo <asadbek.polatov.02@mail.ru>
2022-08-27 20:36:26 +0500

    bug fixed

--- index.php
+++ index.php
@@ -1,51 +1,25 @@
 <?php
 
 include 'Telegram.php';
-$telegram = new Telegram('1210681190:AAFpNO1ochbktqThf4vewibUEESigM04oiA');
+$telegram = new Telegram('5637021086:AAEGBKXf-KhpjA06oVdXgNpT1-2spJdN_hs');
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
-$f = '';
+
+$data = $telegram->getData();
+$message = $data['message'];
+$name = $message['from']['first_name'];
+
 if ($text == '/start') {
-    showMenu();
-} else if ($text == "🌍 Weather") {
-    $content = array('chat_id' => $chat_id, 'text' => "Qayerniki kerak");
-    $telegram->sendMessage($content);
-    $f = $text;
-} else {
-    showWeather();
-}
-
-function showMenu()
-{
-    global $telegram, $chat_id;
     $option = array(
-        array($telegram->buildKeyboardButton("🌍 Weather")),
-//        array($telegram->buildKeyboardButton("❤️ Love", true, false)),
+        array($telegram->buildKeyboardButton("🍯 Ba'tafsil ma'lumot")),
+        array($telegram->buildKeyboardButton("🍯 Buyurtma berish")),
     );
-    $keyb = $telegram->buildKeyBoard($option, $onetime = true, $resize = true);
-    $content = array('chat_id' => $chat_id, 'text' => 'Assalomu alaykum. Botimizga xush kelibsiz!');
-    $telegram->sendMessage($content);
-    $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => 'Tugmalardan birini tanlang');
-    $telegram->sendMessage($content);
-}
-
-function showWeather()
-{
-    global $telegram, $text, $chat_id;
-    $q = $text;
-    $key = "a85c63aee77341ee89b50718223004";
-    $aqi = "yes";
-    $api_url = "http://api.weatherapi.com/v1/current.json?key=" . $key . "&aqi=" . $aqi . "&q=" . $q;
-    $data = json_decode(file_get_contents($api_url), true);
-    $show = " 📍 Name: " . $data['location']['name'] . "\n" .
-        " 📍 Region: " . $data['location']['region'] . "\n" .
-        " 📍 Country: " . $data['location']['country'] . "\n" .
-        " 🌡 Temperature: " . $data['current']['temp_c'] . "\n" .
-        " 🌪 Wind: " . $data['current']['wind_mph'] . "\n" .
-        " 💧 Humidity: " . $data['current']['humidity'] . "\n" .
-        " 🕔 Time: " . $data['current']['last_updated'] . "\n";
-    var_dump($show);
-    $content = array('chat_id' => $chat_id, 'text' => $show, 'parse_mode' => 'html');
+    $keyboard = $telegram->buildKeyBoard($option, true, true);
+    $content = [
+        'chat_id' => $chat_id,
+        'reply_markup'=>$keyboard,
+        'text' => "Assalomu alaykum '$name', Botimizga xush kelibsiz !  Bot orqali masofadan turib 🍯 asal buyurtma qilishingiz mumkin !"
+    ];
     $telegram->sendMessage($content);
 }
 
